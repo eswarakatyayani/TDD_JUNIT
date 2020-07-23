20adf06f6995959f947b7adbe035dde5f4a916c7
eswarakatyayani <37582048+eswarakatyayani@users.noreply.github.com>
2020-07-23 13:12:38 +0530

    Update RemoveFirst.java

--- RemoveFirst.java
+++ RemoveFirst.java
@@ -12,8 +12,11 @@ public class RemoveFirst
 	1.case 1: ABCD  => BCD ---- SUCCESS
 	2.case 2: AACD  => CD ----- SUCCESS
 	3.case 3: BACD  => BCD ---- SUCCESS
-        4.case 4:BBAA   => BBAA --- SUCCESS
-	5.case 5: AABAA => BAA ---- SUCCESS	
+        4.case 4: BBAA  => BBAA --- SUCCESS
+	5.case 5: AABAA => BAA ---- SUCCESS
+	6.case 6: A     => NULL---- SUCCESS
+	7.case 7: AA    => NULL---- SUCCESS 
+	8.case 8: AB    => B   ---- SUCCESS
 	*/
 	
 	@Test
@@ -50,6 +53,28 @@ public class RemoveFirst
 		Removefirstchars r=new Removefirstchars();
 		String s=r.removing("AABAA");
 		assertEquals("BAA",s);
+	}
+	@Test
+	public void testcase6()
+	{
+		Removefirstchars r=new Removefirstchars();
+		String s=r.removing("A");
+		assertEquals("",s);
+	}
+
+	@Test
+	public void testcase7()
+	{
+		Removefirstchars r=new Removefirstchars();
+		String s=r.removing("AA");
+		assertEquals("",s);
+	}
+	@Test
+	public void testcase8()
+	{
+		Removefirstchars r=new Removefirstchars();
+		String s=r.removing("AB");
+		assertEquals("B",s);
 	}
 
 }
